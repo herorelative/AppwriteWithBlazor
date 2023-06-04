2d73b3af116a302c91a6c482ad7e49b04c467578
herorelative <herorelative@gmail.com>
2023-06-04 03:40:33 +0000

    Deploying to github-pages from @ herorelative/AppwriteWithBlazor@c0478733f8957ddfaf76952f4956ce496b8855a2 🚀

---
+++ interop.js
@@ -0,0 +1,11 @@
+﻿window.localStorage = {
+    setItem: (key, value) => {
+        localStorage.setItem(key, value);
+    },
+    getItem: (key) => {
+        return localStorage.getItem(key);
+    },
+    removeItem: (key) => {
+        localStorage.removeItem(key);
+    }
+};
